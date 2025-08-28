37f0cc97d9e4b953a9591aae75c9d1997a7fff4c
Irfancpv99 <irfancpv99@gmail.com>
2025-08-28 17:01:13 +0200

    REFACTOR: Improved Todo class with validation and object methods- Add null validation for description- Add setDescription method with validation- Add equals, hashCode, toString methods- Add comprehensive tests for new functionality

--- src/main/java/com/todoapp/model/Todo.java
+++ src/main/java/com/todoapp/model/Todo.java
@@ -1,5 +1,6 @@
 package com.todoapp.model;
 
+import java.util.Objects;
 
 public class Todo {
     
@@ -8,8 +9,11 @@ public class Todo {
     private boolean done;
     
     public Todo(String description) {
+        if (description == null) {
+            throw new IllegalArgumentException("Description cannot be null");
+        }
         this.description = description;
-        this.done = false; // Default to not done
+        this.done = false;
     }
     
     public Long getId() {
@@ -24,11 +28,49 @@ public class Todo {
         return description;
     }
     
+    public void setDescription(String description) {
+        if (description == null) {
+            throw new IllegalArgumentException("Description cannot be null");
+        }
+        this.description = description;
+    }
+    
     public boolean isDone() {
         return done;
     }
     
     public void setDone(boolean done) {
         this.done = done;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Todo todo = (Todo) obj;
+        
+         if (id != null && todo.id != null) {
+            return Objects.equals(id, todo.id);
+        }
+        
+         if (id == null && todo.id == null) {
+            return false;
+        }
+        
+        return false;
+    }
+    
+    @Override
+    public int hashCode() {
+        return id != null ? Objects.hash(id) : System.identityHashCode(this);
+    }
+    
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", done=" + done +
+                '}';
     }
 }
--- src/test/java/com/todoapp/model/TodoTest.java
+++ src/test/java/com/todoapp/model/TodoTest.java
@@ -7,7 +7,6 @@ class TodoTest {
 
     @Test
     void shouldCreateTodoWithDescription() {
-        
         String description = "Learn TDD methodology";
         
         Todo todo = new Todo(description);
@@ -18,7 +17,6 @@ class TodoTest {
     
     @Test
     void shouldHaveNullIdWhenCreated() {
-        // RED: This will fail - Todo don't have getId() method yet
         Todo todo = new Todo("Test task");
         
         assertNull(todo.getId());
@@ -26,7 +24,6 @@ class TodoTest {
     
     @Test
     void shouldBeNotDoneByDefault() {
-        // RED: This will fail - Todo don't have isDone() method yet
         Todo todo = new Todo("Test task");
         
         assertFalse(todo.isDone());
@@ -34,7 +31,6 @@ class TodoTest {
     
     @Test
     void shouldAllowSettingId() {
-        // RED: This will fail - Todo don't have setId() method yet
         Todo todo = new Todo("Test task");
         
         todo.setId(1L);
@@ -44,11 +40,94 @@ class TodoTest {
     
     @Test
     void shouldAllowMarkingAsDone() {
-        // RED: This will fail - Todo don't have setDone() method yet
         Todo todo = new Todo("Test task");
         
         todo.setDone(true);
         
         assertTrue(todo.isDone());
+    }
+    
+    // REFACTOR PHASE
+    @Test
+    void shouldThrowExceptionWhenCreatedWithNullDescription() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Todo(null);
+        });
+    }
+    
+    @Test
+    void shouldAllowSettingDescription() {
+        Todo todo = new Todo("Original description");
+        
+        todo.setDescription("Updated description");
+        
+        assertEquals("Updated description", todo.getDescription());
+    }
+    
+    @Test
+    void shouldThrowExceptionWhenSettingNullDescription() {
+        Todo todo = new Todo("Test task");
+        
+        assertThrows(IllegalArgumentException.class, () -> {
+            todo.setDescription(null);
+        });
+    }
+    
+    @Test
+    void shouldBeEqualWhenSameId() {
+        Todo todo1 = new Todo("Task 1");
+        Todo todo2 = new Todo("Task 2");
+        
+        todo1.setId(1L);
+        todo2.setId(1L);
+        
+        assertEquals(todo1, todo2);
+        assertEquals(todo1.hashCode(), todo2.hashCode());
+    }
+    
+    @Test
+    void shouldNotBeEqualWhenDifferentId() {
+        Todo todo1 = new Todo("Task 1");
+        Todo todo2 = new Todo("Task 2");
+        
+        todo1.setId(1L);
+        todo2.setId(2L);
+        
+        assertNotEquals(todo1, todo2);
+    }
+    
+    @Test
+    void shouldNotBeEqualWhenNoId() {
+        Todo todo1 = new Todo("Task 1");
+        Todo todo2 = new Todo("Task 1");
+        
+        assertNotEquals(todo1, todo2);
+    }
+    
+    @Test
+    void shouldNotBeEqualToNull() {
+        Todo todo = new Todo("Test task");
+        
+        assertNotEquals(todo, null);
+    }
+    
+    @Test
+    void shouldNotBeEqualToDifferentClass() {
+        Todo todo = new Todo("Test task");
+        
+        assertNotEquals(todo, "not a todo");
+    }
+    
+    @Test
+    void shouldHaveStringRepresentation() {
+        Todo todo = new Todo("Test task");
+        todo.setId(1L);
+        todo.setDone(true);
+        
+        String result = todo.toString();
+        
+        assertTrue(result.contains("id=1"));
+        assertTrue(result.contains("description='Test task'"));
+        assertTrue(result.contains("done=true"));
     }
 }
