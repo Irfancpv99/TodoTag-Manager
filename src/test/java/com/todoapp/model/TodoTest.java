42327fe7108c36e42fa648780605fd99b7ef1cc7
Irfancpv99 <irfancpv99@gmail.com>
2025-08-28 16:33:22 +0200

    RED: Add failing tests for Todo ID and done status- Test for null ID when Todo created- Test for default not-done status- Test for setting ID- Test for marking as done- All new tests will fail - methods don't exist yet

--- src/test/java/com/todoapp/model/TodoTest.java
+++ src/test/java/com/todoapp/model/TodoTest.java
@@ -3,16 +3,55 @@ package com.todoapp.model;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * TDD Test for Todo entity - Extended tests
+ * 
+ * Adding new failing tests for ID and done status
+ */
 class TodoTest {
 
     @Test
     void shouldCreateTodoWithDescription() {
-        // RED: This will fail - Todo class doesn't exist yet
+        
         String description = "Learn TDD methodology";
         
         Todo todo = new Todo(description);
         
         assertNotNull(todo);
         assertEquals("Learn TDD methodology", todo.getDescription());
+    }
+    
+    
+    @Test
+    // RED: This will fail - Todo don't have getId() method yet
+    void shouldHaveNullIdWhenCreated() {
+        Todo todo = new Todo("Test task");
+        assertNull(todo.getId());
+    }
+    
+    @Test
+    // RED: This will fail - Todo don't have isDone() method yet
+    
+    void shouldBeNotDoneByDefault() {
+        Todo todo = new Todo("Test task");
+        assertFalse(todo.isDone());
+    }
+    
+    @Test
+    void shouldAllowSettingId() {
+    // RED: This will fail - Todo don't have setId() method yet
+    
+    	Todo todo = new Todo("Test task");
+        todo.setId(1L);
+        assertEquals(1L, todo.getId());
+    }
+    
+    @Test
+    void shouldAllowMarkingAsDone() {
+    // RED: This will fail - Todo don't have setDone() method yet
+        
+    	Todo todo = new Todo("Test task");
+        todo.setDone(true);
+        assertTrue(todo.isDone());
     }
 }
