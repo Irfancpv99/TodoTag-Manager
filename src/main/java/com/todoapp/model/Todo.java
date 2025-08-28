3cb046bd07a2d22884f52b59feb720ff01d1dc5c
Irfancpv99 <irfancpv99@gmail.com>
2025-08-28 16:17:45 +0200

    GREEN: Implement minimal Todo entity to pass test- Add Todo class with constructor and getDescription()- Minimal implementation - just enough to pass test- Test should now pass: Todo creation with description

---
+++ src/main/java/com/todoapp/model/Todo.java
@@ -0,0 +1,14 @@
+package com.todoapp.model;
+
+public class Todo {
+    
+    private String description;
+    
+    public Todo(String description) {
+        this.description = description;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+}
