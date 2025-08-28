455f3ffd77138dd8fea30f1597e03835fd892a95
Irfancpv99 <irfancpv99@gmail.com>
2025-08-28 16:37:00 +0200

    GREEN: Add ID and done fields to Todo entity- Add Long id field with getter/setter- Add boolean done field with getter/setter- Set done=false by default in constructor- All tests should now pass

--- src/main/java/com/todoapp/model/Todo.java
+++ src/main/java/com/todoapp/model/Todo.java
@@ -2,13 +2,32 @@ package com.todoapp.model;
 
 public class Todo {
     
+    private Long id;
     private String description;
+    private boolean done;
     
     public Todo(String description) {
         this.description = description;
+        this.done = false; 
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
     }
     
     public String getDescription() {
         return description;
+    }
+    
+    public boolean isDone() {
+        return done;
+    }
+    
+    public void setDone(boolean done) {
+        this.done = done;
     }
 }
