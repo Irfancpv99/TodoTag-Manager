0a1fd6ce88ad9d2db91b67bc75d25e4fe4c272ad
Irfancpv99 <irfancpv99@gmail.com>
2025-09-01 17:38:02 +0200

    removed config folder

--- src/test/java/com/todoapp/config/AppConfigTest.java
+++ src/test/java/com/todoapp/config/AppConfigTest.java
@@ -2,37 +2,28 @@ package com.todoapp.config;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Field;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * TDD Test for AppConfig
+ * 
+ * RED PHASE: These tests will fail because AppConfig and DatabaseType don't exist yet.
+ * Following TDD: test-driven configuration design
+ */
 class AppConfigTest {
 
     private AppConfig config;
 
     @BeforeEach
-    void setUp() throws Exception {
-        // Reset singleton instance before each test
-        resetSingleton();
+    void setUp() {
+        // RED: This will fail - AppConfig class doesn't exist yet
         config = AppConfig.getInstance();
-    }
-
-    private void resetSingleton() throws Exception {
-        Field instanceField = AppConfig.class.getDeclaredField("instance");
-        instanceField.setAccessible(true);
-        instanceField.set(null, null);
     }
 
     @Test
     void shouldBeSingleton() {
+        // RED: This will fail - AppConfig doesn't exist yet
         AppConfig instance1 = AppConfig.getInstance();
         AppConfig instance2 = AppConfig.getInstance();
         
@@ -41,14 +32,17 @@ class AppConfigTest {
 
     @Test
     void shouldHaveDefaultDatabaseType() {
+        // RED: This will fail - DatabaseType enum doesn't exist yet
         DatabaseType dbType = config.getDatabaseType();
         
         assertNotNull(dbType);
+        // Default should be MongoDB
         assertEquals(DatabaseType.MONGODB, dbType);
     }
 
     @Test
     void shouldAllowChangingDatabaseType() {
+        // RED: This will fail - methods don't exist yet
         config.setDatabaseType(DatabaseType.MYSQL);
         assertEquals(DatabaseType.MYSQL, config.getDatabaseType());
         
@@ -58,6 +52,7 @@ class AppConfigTest {
 
     @Test
     void shouldProvideMongoDbConfiguration() {
+        // RED: This will fail - methods don't exist yet
         assertEquals("localhost", config.getMongoDbHost());
         assertEquals(27017, config.getMongoDbPort());
         assertEquals("todoapp", config.getMongoDbDatabase());
@@ -65,6 +60,7 @@ class AppConfigTest {
 
     @Test
     void shouldProvideMySqlConfiguration() {
+        // RED: This will fail - methods don't exist yet
         assertNotNull(config.getMySqlUrl());
         assertNotNull(config.getMySqlUsername());
         assertNotNull(config.getMySqlPassword());
@@ -72,179 +68,5 @@ class AppConfigTest {
         assertTrue(config.getMySqlUrl().contains("jdbc:mysql"));
         assertEquals("todouser", config.getMySqlUsername());
         assertEquals("todopassword", config.getMySqlPassword());
-    }
-
-    // Additional tests to achieve 100% mutation coverage
-
-    @Test
-    void shouldHandleNullDatabaseTypeGracefully() {
-        // Test the default value fallback in getDatabaseType()
-        Properties props = getPropertiesField();
-        props.remove("database.type");
-        
-        assertEquals(DatabaseType.MONGODB, config.getDatabaseType());
-    }
-
-    @Test
-    void shouldHandleLowercaseDatabaseType() throws Exception {
-        resetSingleton();
-        
-        // Create a mock properties file with lowercase database type
-        String propertiesContent = "database.type=mysql\n";
-        createMockPropertiesFile(propertiesContent);
-        
-        AppConfig testConfig = AppConfig.getInstance();
-        assertEquals(DatabaseType.MYSQL, testConfig.getDatabaseType());
-    }
-
-    @Test
-    void shouldUseDefaultValuesWhenPropertiesFileNotFound() throws Exception {
-        resetSingleton();
-        
-        // This will trigger the default properties path since no properties file exists
-        AppConfig testConfig = AppConfig.getInstance();
-        
-        assertEquals(DatabaseType.MONGODB, testConfig.getDatabaseType());
-        assertEquals("localhost", testConfig.getMongoDbHost());
-        assertEquals(27017, testConfig.getMongoDbPort());
-        assertEquals("todoapp", testConfig.getMongoDbDatabase());
-        assertNotNull(testConfig.getMySqlUrl());
-        assertNotNull(testConfig.getMySqlUsername());
-        assertNotNull(testConfig.getMySqlPassword());
-    }
-
-    @Test
-    void shouldHandleInvalidPortNumber() {
-        Properties props = getPropertiesField();
-        String originalPort = props.getProperty("mongodb.port");
-        
-        // Test with default port when property is missing
-        props.remove("mongodb.port");
-        assertEquals(27017, config.getMongoDbPort());
-        
-        // Restore original value
-        if (originalPort != null) {
-            props.setProperty("mongodb.port", originalPort);
-        }
-    }
-
-    @Test
-    void shouldHandleEmptyPropertyValues() {
-        Properties props = getPropertiesField();
-        
-        // Test defaults when properties are empty
-        props.setProperty("mongodb.host", "");
-        props.setProperty("mongodb.database", "");
-        
-        // Should still return the empty string, not the default
-        assertEquals("", config.getMongoDbHost());
-        assertEquals("", config.getMongoDbDatabase());
-        
-        // Reset to defaults
-        props.setProperty("mongodb.host", "localhost");
-        props.setProperty("mongodb.database", "todoapp");
-    }
-
-    @Test
-    void shouldReturnNullForMissingMySqlProperties() {
-        Properties props = getPropertiesField();
-        
-        // Remove MySQL properties to test null returns
-        String originalUrl = props.getProperty("mysql.url");
-        String originalUsername = props.getProperty("mysql.username");
-        String originalPassword = props.getProperty("mysql.password");
-        
-        props.remove("mysql.url");
-        props.remove("mysql.username");
-        props.remove("mysql.password");
-        
-        assertNull(config.getMySqlUrl());
-        assertNull(config.getMySqlUsername());
-        assertNull(config.getMySqlPassword());
-        
-        // Restore original values
-        if (originalUrl != null) props.setProperty("mysql.url", originalUrl);
-        if (originalUsername != null) props.setProperty("mysql.username", originalUsername);
-        if (originalPassword != null) props.setProperty("mysql.password", originalPassword);
-    }
-
-    @Test
-    void shouldTestIOExceptionHandling() throws Exception {
-        resetSingleton();
-        
-        // This test ensures the IOException catch block is covered
-        // by creating a scenario where properties loading might fail
-        AppConfig testConfig = AppConfig.getInstance();
-        
-        // Verify default properties are set when IO exception occurs
-        assertNotNull(testConfig.getDatabaseType());
-        assertNotNull(testConfig.getMongoDbHost());
-    }
-
-    @Test
-    void shouldTestAllDatabaseTypeEnumValues() {
-        // Ensure both enum values work correctly
-        config.setDatabaseType(DatabaseType.MONGODB);
-        assertEquals(DatabaseType.MONGODB, config.getDatabaseType());
-        
-        config.setDatabaseType(DatabaseType.MYSQL);
-        assertEquals(DatabaseType.MYSQL, config.getDatabaseType());
-        
-        // Test that enum valueOf works with exact case
-        Properties props = getPropertiesField();
-        props.setProperty("database.type", "MONGODB");
-        assertEquals(DatabaseType.MONGODB, config.getDatabaseType());
-        
-        props.setProperty("database.type", "MYSQL");
-        assertEquals(DatabaseType.MYSQL, config.getDatabaseType());
-    }
-
-    @Test
-    void shouldTestSingletonThreadSafety() throws Exception {
-        resetSingleton();
-        
-        final AppConfig[] instances = new AppConfig[2];
-        final Thread[] threads = new Thread[2];
-        
-        for (int i = 0; i < 2; i++) {
-            final int index = i;
-            threads[i] = new Thread(() -> instances[index] = AppConfig.getInstance());
-        }
-        
-        for (Thread thread : threads) {
-            thread.start();
-        }
-        
-        for (Thread thread : threads) {
-            thread.join();
-        }
-        
-        assertSame(instances[0], instances[1]);
-    }
-
-    // Helper methods
-
-    private Properties getPropertiesField() {
-        try {
-            Field propertiesField = AppConfig.class.getDeclaredField("properties");
-            propertiesField.setAccessible(true);
-            return (Properties) propertiesField.get(config);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to access properties field", e);
-        }
-    }
-
-    private void createMockPropertiesFile(String content) {
-        // This method simulates loading properties from a string
-        // In a real scenario, you might need to mock the ClassLoader
-        // For this test, we'll directly set the properties
-        Properties props = getPropertiesField();
-        Properties mockProps = new Properties();
-        try (InputStream is = new ByteArrayInputStream(content.getBytes())) {
-            mockProps.load(is);
-            props.putAll(mockProps);
-        } catch (IOException e) {
-            fail("Failed to create mock properties");
-        }
     }
 }
