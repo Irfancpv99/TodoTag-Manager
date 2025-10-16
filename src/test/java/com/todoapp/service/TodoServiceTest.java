6f15130b0e62e17b5e4bd63c3a3d1cc9042464c3
Irfancpv99 <irfancpv99@gmail.com>
2025-10-16 15:37:25 +0200

    RED : test for Get todo by ID

--- src/test/java/com/todoapp/service/TodoServiceTest.java
+++ src/test/java/com/todoapp/service/TodoServiceTest.java
@@ -1,9 +1,12 @@
 package com.todoapp.service;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
@@ -30,5 +33,16 @@ class TodoServiceTest {
         when(todoRepository.findAll()).thenReturn(todos);
 
         assertEquals(todos, todoService.getAllTodos());
+    }
+    
+    @Test
+    void shouldGetTodoById() {
+        Todo todo = new Todo("Task 1");
+        when(todoRepository.findById(1L)).thenReturn(Optional.of(todo));
+
+        Optional result = todoService.getTodoById(1L);
+
+        assertTrue(result.isPresent());
+        assertEquals(todo, result.get());
     }
 }
