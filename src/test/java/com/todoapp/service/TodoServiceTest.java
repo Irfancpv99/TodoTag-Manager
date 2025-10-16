52cfe92d373c8cc7537fa0e29d92ff7766ea4959
Irfancpv99 <irfancpv99@gmail.com>
2025-10-16 15:32:34 +0200

    RED: implement getAllTodos functionality

--- src/test/java/com/todoapp/service/TodoServiceTest.java
+++ src/test/java/com/todoapp/service/TodoServiceTest.java
@@ -1,8 +1,14 @@
 package com.todoapp.service;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 
+import com.todoapp.model.Todo;
 import com.todoapp.repository.TagRepository;
 import com.todoapp.repository.TodoRepository;
 
@@ -16,5 +22,13 @@ class TodoServiceTest {
         todoRepository = mock(TodoRepository.class);
         tagRepository = mock(TagRepository.class);
         todoService = new TodoService(todoRepository, tagRepository);
+    }
+    
+    @Test
+    void shouldGetAllTodos() {
+        List todos = List.of(new Todo("Task 1"));
+        when(todoRepository.findAll()).thenReturn(todos);
+
+        assertEquals(todos, todoService.getAllTodos());
     }
 }
